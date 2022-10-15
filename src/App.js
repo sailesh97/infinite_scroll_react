9e6e15aee22c044bb7c36731da4f0b7d8de0c75c
sailesh97 <saileshpatra997@gmail.com>
2022-10-15 14:47:32 +0530

    Adding intersection observer to last element ref and incrementing page number when user scrolled to last element

--- src/App.js
+++ src/App.js
@@ -4,18 +4,35 @@ import useBookSearch from "./useBookSearch";
 function App() {
   const [query, setQuery] = useState('');
   const [pageNumber, setPageNumber] = useState(1);
+  
+  const {loading, error, books, hasMore} = useBookSearch(query, pageNumber);
 
-  const observer = useRef();
+  const observer = useRef(); // #nullRef
   const lastBookElementRef = useCallback(node => {
-    console.log(node)
-  });
+    if(loading) return;
+    if(observer.current) observer.current.disconnect()
+    /** 
+     * If check is neccessary as in 1st load observer will be null at #nullRef 
+     * 
+     * Disconnecting old bookElementRef, so that we can connect the new lastBookElement with ref properly.
+     * 
+     * */
+    
+    observer.current = new IntersectionObserver(entries => {
+      // entries[0].isIntersecting will be true only when you visit the last book element rendered in page.
+      if(entries[0].isIntersecting && hasMore) {
+        console.log('Visible');
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if(node) observer.current.observe(node)
+  }, [loading, hasMore]);
 
   function handleSearch(e){
     setQuery(e.target.value);
     setPageNumber(1)
   }
 
-  const {loading, error, books, hasMore} = useBookSearch(query, pageNumber);
 
   return (
     <>
