32f45460d903ad447d64e09d4b28384754a82d2a
sailesh97 <saileshpatra997@gmail.com>
2022-10-15 13:45:33 +0530

    Sending request to openlibrary api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { useState } from "react";
+import useBookSearch from "./useBookSearch";
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function handleSearch(e){
+    setQuery(e.target.value);
+    setPageNumber(1)
+  }
+
+  useBookSearch(query, pageNumber);
+  
   return (
     <>
-      <input type='text' />
+      <input type='text' onChange={handleSearch}/>
       <div>Title</div>
       <div>Title</div>
       <div>Title</div>
---
+++ src/useBookSearch.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function useBookSearch(query, pageNumber) {
+
+    useEffect(()=> {
+        axios({
+            method: 'GET',
+            url: 'http://openlibrary.org/search.json',
+            params: {q: query, page: pageNumber}
+        }).then(res => {
+            console.log(res.data)
+        })
+    },[query, pageNumber]);
+
+  return null
+}
