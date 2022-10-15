8ef287fb03ab65a901c77a14b6132ba7fb66890f
sailesh97 <saileshpatra997@gmail.com>
2022-10-15 13:50:56 +0530

    Cancelling previous requests

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -4,13 +4,19 @@ import axios from 'axios';
 export default function useBookSearch(query, pageNumber) {
 
     useEffect(()=> {
+        let cancel;
         axios({
             method: 'GET',
             url: 'http://openlibrary.org/search.json',
-            params: {q: query, page: pageNumber}
+            params: {q: query, page: pageNumber},
+            cancelToken: new axios.CancelToken(c => cancel = c)
         }).then(res => {
             console.log(res.data)
+        }).catch(e => {
+            if(axios.isCancel(e)) return;
         })
+
+        return () => cancel()
     },[query, pageNumber]);
 
   return null
